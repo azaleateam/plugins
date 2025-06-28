1ab3251a5713e592806a58dd3f70bf7854bb17a8
honkling <untitledgeese@gmail.com>
2025-06-27 20:23:51 -0500

    update kotlin and minestom. shade 4koma

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.2.0"
+//    id("com.gradleup.shadow") version "8.3.2"
     `maven-publish`
 }
 
-val versionNumber = "0.1.1"
 group = "team.azalea"
-version = versionNumber
+version = "0.1.2"
 
 repositories {
     mavenCentral()
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("net.minestom:minestom-snapshots:9803f2bfe3")
+    compileOnly("net.minestom:minestom-snapshots:4fe2993057")
     implementation("cc.ekblad:4koma:1.2.0")
 }
 
@@ -21,14 +21,23 @@ kotlin {
     jvmToolchain(21)
 }
 
+tasks.build {
+    dependsOn("shadowJar", "publishToMavenLocal")
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "team.azalea"
+            groupId = project.group.toString()
             artifactId = "plugins"
-            version = versionNumber
+            version = project.version.toString()
 
             from(components["java"])
         }
+    }
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
     }
 }
